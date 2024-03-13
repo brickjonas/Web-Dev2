ed18bc3bb9bf66bb53876236699b54ac7ff60ec6
Andrew Boghosian <boghosian03@gmail.com>
2024-03-13 00:46:10 -0700

    tweaks

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.querySelector("#search").addEventListener("click", getPokemon);
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function getPokemon(e) {
     const name = document.querySelector("#pokemonName").value;
+    const pokeName = name.toLowerCase();
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
         .then((response) => response.json())
         .then((data) => {
             document.querySelector(".pokemonBox").innerHTML = `
@@ -14,8 +19,8 @@ function getPokemon(e) {
                 />
             </div>
             <div class="pokemonInfo">
-                <h1>${data.name}</h1>
-                <p>Weight:${data.weight}</p>
+                <h1>${capitalizeFirstLetter(data.name)}</h1>
+                <p>Weight:${parseInt(data.weight * 0.220462)} lbs</p>
             </div>
                 `;
         })
@@ -25,5 +30,7 @@ function getPokemon(e) {
 
     e.preventDefualt();
 }
+
+
 
 getPokemon();
