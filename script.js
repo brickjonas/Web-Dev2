db154f6fb6fcce141129c499c1c1c74acf3eeaa9
Huy Nguyen <157776754+huyn9uy3n@users.noreply.github.com>
2024-03-13 11:36:07 -0700

    Update script.js
    - search bar works in the real time now
    - print error if the ID or pokemon name is wrong

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
+const maxPokemon = 1025;
+
 // When click the seach icon
 document.querySelector("#search").addEventListener("click", getPokemon);
 
 // When hit Enter in search bar
-document.getElementById('pokemonName').addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
+document.getElementById('pokemonName').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
         getPokemon(); 
     }
 });
 
+// While typing in search bar
+document.getElementById('pokemonName').addEventListener('input', () => {
+    getPokemon();
+  });
+
+// Capitalize the first letter (for printting Pokemon Name)
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase()+string.slice(1);
 }
 
-function getPokemon(e) {
-    const name = document.querySelector("#pokemonName").value.toLowerCase();
+// fetch from Pokemon API and display the result
+function getPokemon(e){
+    const pokemonName = document.querySelector("#pokemonName").value.toLowerCase();
 
-    fetch (`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then((response) => response.json())
         .then((data) => {
             document.querySelector(".pokemonBox").innerHTML = `
             <div>
-                <img
-                    src="${data.sprites.other["official-artwork"].front_default}"
-                    alt="${data.name}"
-                />
+                <h1>${capitalizeFirstLetter(data.name)}</h1>
+                <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
             </div>
             <div class="pokemonInfo">
-                <h1>${capitalizeFirstLetter(data.name)}</h1>
+                
                 <p>#${data.id}</p>
                 <p>Height: ${data.height}</p>
-                <p>Weight: ${parseInt(data.weight * 0.220462)} lbs</p>
+                <p>Weight: ${data.weight}</p>
             </div>
             `;
         })
-        .catch((err) => {
-            console.log("Pokemon not found", err);
+        .catch(error => {
+            document.querySelector(".pokemonBox").innerHTML = `
+            <div>
+            <h1>Wrong ID or Pokemon Name, Try again</h1>
+            </div>
+            `;
         });
-
-    //e.preventDefualt();
 }
-
-//testing to make my own branch lol
-
-
-
-getPokemon();
