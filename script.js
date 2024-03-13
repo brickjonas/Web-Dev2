c8832bde2af42cb030c015d76c8fa41aa3c7d66c
Andrew Boghosian <boghosian03@gmail.com>
2024-03-13 15:43:14 -0700

    same as last commit

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function getPokemon(e){
                 
                 <p>#${data.id}</p>
                 <p>Height: ${data.height}</p>
-                <p>Weight: ${parseInt(data.weight * 0.220462)}</p>
+                <p>Weight: ${parseInt(data.weight * 0.220462)} lbs</p>
             </div>
             `;
         })
