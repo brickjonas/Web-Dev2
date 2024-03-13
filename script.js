7a1e9bb813fada31679f08937d65fff721174da0
Huy Nguyen <157776754+huyn9uy3n@users.noreply.github.com>
2024-03-12 22:59:08 -0700

    Update script.js
    - lowercase and uppercase letters work in the search bar now.
    - add ID and height of the pokemon

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 document.querySelector("#search").addEventListener("click", getPokemon);
 
-function getPokemon(e) {
-    const name = document.querySelector("#pokemonName").value;
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase()+string.slice(1);
+}
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+function getPokemon(e){
+    const pokemonName = document.querySelector("#pokemonName").value.toLowerCase();
+
+    fetch (`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then((response) => response.json())
         .then((data) => {
             document.querySelector(".pokemonBox").innerHTML = `
             <div>
-                <img
-                    src="${data.sprites.other["official-artwork"].front_default}"
-                    alt="${data.name}"
-                />
+                <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
             </div>
             <div class="pokemonInfo">
-                <h1>${data.name}</h1>
-                <p>Weight:${data.weight}</p>
+                <h1>${capitalizeFirstLetter(data.name)}</h1>
+                <p>#${data.id}</p>
+                <p>Height: ${data.height}</p>
+                <p>Weight: ${data.weight}</p>
             </div>
-                `;
+            `;
         })
-        .catch((err) => {
-            console.log("Pokemon not found", err);
-        });
-
-    e.preventDefualt();
+        .catch((err) => console.log("Pokemon not found", err));
 }
-
-getPokemon();
